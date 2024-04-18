c57431a9ef8f8784263797af2c5902f570663743
Nick Lopez <116652056+FenrirIsFree@users.noreply.github.com>
2024-04-17 20:27:03 -0400

    Added Birb

--- Assets/Scripts/SlingShotHandler.cs
+++ Assets/Scripts/SlingShotHandler.cs
@@ -6,27 +6,59 @@ using UnityEngine.InputSystem;
 
 public class SlingShotHandler : MonoBehaviour
 {
+    [Header("Line Renderer")]
     [SerializeField] private LineRenderer _leftLineRenderer;
     [SerializeField] private LineRenderer _rightLineRenderer;
 
+    [Header("Transform References")]
     [SerializeField] private Transform _leftStartPosition;
     [SerializeField] private Transform _rightStartPosition;
     [SerializeField] private Transform _centerPosition;
     [SerializeField] private Transform _idlePosition;
 
+    [Header("Slingshot Stats")]
     [SerializeField] private float _maxDistance = 3.5f;
+
+    [Header("Scripts")]
+    [SerializeField] private SlingShotArea _slingshotArea;
 
     private Vector2 _slingShotLinesPosition;
 
-    private void Update() {
-        if (Mouse.current.leftButton.isPressed) {
+    private bool _clickedWithinArea;
+
+    private void Awake() 
+    {
+        _leftLineRenderer.enabled = false;
+        _rightLineRenderer.enabled = false;
+    }
+
+    private void Update()
+    {
+
+        if (Mouse.current.leftButton.wasPressedThisFrame && _slingshotArea.IsWithinSlingShotArea())
+        {
+            _clickedWithinArea = true;
+        }
+
+        if (Mouse.current.leftButton.isPressed && _clickedWithinArea)
+        {
             DrawSlingShot();
         }
 
-        Debug.Log(Mouse.current.position.ReadValue());
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+           _clickedWithinArea = false; 
+        }
     }
 
-    private void DrawSlingShot() {
+    private void DrawSlingShot()
+    {
+        if (!_leftLineRenderer.enabled && !_rightLineRenderer.enabled)
+        {
+            _leftLineRenderer.enabled = true;
+            _rightLineRenderer.enabled = true;
+        }
+
         Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
         _slingShotLinesPosition = _centerPosition.position + Vector3.ClampMagnitude(touchPosition - _centerPosition.position, _maxDistance);
@@ -34,7 +66,8 @@ public class SlingShotHandler : MonoBehaviour
         SetLines(_slingShotLinesPosition);
     }
 
-    private void SetLines(Vector2 position) {
+    private void SetLines(Vector2 position)
+    {
         _leftLineRenderer.SetPosition(0, position);
         _leftLineRenderer.SetPosition(1, _leftStartPosition.position);
 
